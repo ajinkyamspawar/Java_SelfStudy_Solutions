6bb7a54aa51d71503c162afb7a2b6fc920ede04f
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-03-03 00:06:12 +0530

    Add files via upload

--- SelfStudy/FindDivisors.java
+++ SelfStudy/FindDivisors.java
@@ -2,25 +2,27 @@ package selfStudy;
 
 import java.util.Scanner;
 
-public class FindDivisors {
+public class FindDivisors 
+{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number to find out it's all the divisors");
 		int number = sc.nextInt();
-		int i =1;
-		for ( ; i<=number; i++)
+		int count = 0;
+		for ( int i =1; i<=number; i++)
 		{
-		if(number % i == 0)
-		{
+			if(number % i == 0) // It will check whether the number is divisible by assigned value of i or not. 
+			{
+			count++;
+			System.out.print(i+","); // If the number is divisible by the assigned value of i then it will print the number. 
 			
-			System.out.print(i+",");
-			
-		}
+			}
 		
 		
 		}
-		
+		System.out.println("\nTotal Number of divisors are:" +count); // Returns the total number of integers. 
 		
 	}
 	
